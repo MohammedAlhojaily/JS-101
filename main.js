3c36126b8a5d11a86e2d2693cd437d79d5f40f39
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-19 17:41:55 +0300

    Else If

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let age = 15;
-if(age < 12 ){
-    console.log('Sorry, you are not old enough to play thsi game');
+let number = 0;
+
+if(number > 0){
+    console.log('+');
+}else if(number < 0){
+    console.log('-');
 }else{
-    console.log('Welcome to the game');
+    console.log('zero');
 }
