0eb2badc47de67270c4ad0ea67716b821bdd9bce
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-19 17:46:18 +0300

    For loop

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const colors = ['red', 'green', 'blue'];
-let colorNumber = 2;
-
-switch(colorNumber){
-    case 1:
-        console.log(colors[0]);
-        break;
-    case 2:
-        console.log(colors[1]);
-        break;
-    case 3:
-         console.log(colors[2]);
-         break;
-    case 4:
-    case 5:
-    case 6:
-        console.log('black');
-        break;
-    default:
-        console.log('white');
-        break;
+for(let i = 1; i <= 50; i++){
+    console.log(i);
 }
