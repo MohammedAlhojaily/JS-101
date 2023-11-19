7ecfd7c7196158943950dcf36796854dac5df9ed
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-19 17:38:47 +0300

    Split Method

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const values = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-console.log(values[2][2]);
+let message = 'Welcome to JavaScript';
+let result = message.split(' ');
+console.log(result);
